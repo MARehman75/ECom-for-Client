105b78e137b72030c03bc3e4de483cf5a878de83
MARehman75 <rehman.hanif16@gmail.com>
2023-12-17 01:40:32 +0500

    Checkout page Added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import CartPage from './pages/CartPage';
+import Checkout from './pages/Checkout';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
@@ -27,6 +28,10 @@ const router = createBrowserRouter([
   {
     path: "/cart",
     element: <CartPage/>,
+  },
+  {
+    path: "/checkout",
+    element: <Checkout/>,
   },
 ]);
 

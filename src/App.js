4634f94cbb920d11b85c27dd119c5fc19c0e48c4
MARehman75 <rehman.hanif16@gmail.com>
2023-12-16 19:53:43 +0500

    Login Signup added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      {/* <Home/> */}
+      {/* <LoginPage/> */}
+      <SignupPage/>
     </div>
   );
 }

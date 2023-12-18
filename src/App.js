58f0a6bf52bae9f3b4d094768de350daa2dbb9f6
MARehman75 <rehman.hanif16@gmail.com>
2023-12-18 06:25:00 +0500

    Product Detail page added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CartPage from './pages/CartPage';
 import Checkout from './pages/Checkout';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
+import ProductDetailPage from './pages/ProductDetailPage';
 import SignupPage from './pages/SignupPage';
 
 import {
@@ -32,6 +33,10 @@ const router = createBrowserRouter([
   {
     path: "/checkout",
     element: <Checkout/>,
+  },
+  {
+    path: "/product-detail",
+    element: <ProductDetailPage/>,
   },
 ]);
 
